0c9958cba903198be0ca85858c3aa46306d7f836
Vasiliy <gladishevvasiliy@gmail.com>
2017-11-10 14:39:21 +0300

    eslint works

--- frontend/src/components/SignIn/index.js
+++ frontend/src/components/SignIn/index.js
@@ -3,19 +3,18 @@ import ReactDOM from 'react-dom';
 
 class SignInForm extends React.Component {
   constructor() {
-  super();
-  this.state = {
-    username: '',
-    password: ''
+    super();
+    this.state = {
+      username: '',
+      password: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  this.handleChange = this.handleChange.bind(this);
-  this.handleSubmit = this.handleSubmit.bind(this);
-}
-
   handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   handleSubmit(e) {
@@ -25,22 +24,22 @@ class SignInForm extends React.Component {
       body: JSON.stringify({
         username: this.state.username,
         password: this.state.username,
-      })
-    }).then(() => {alert("Succesful")})
-    .catch(() => {alert("Error")});
+      }),
+    }).then(() => { alert('Succesful') }) // eslint-disable-line no-alert
+      .catch(() => { alert('Error') }); // eslint-disable-line no-alert
     // alert(this.state.username +" " + this.state.password);
   }
 
 
-  render(){
+  render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <h1>Sign In</h1>
-        <label>
-          <input name="username" type="text" value={this.state.username} onChange={this.handleChange}/>
-          <input name="password" type="password" value={this.state.password} onChange={this.handleChange}/>
-      </label>
-        <input type="submit" value="Sign In"/>
+        <label htmlFor="SignInForm">
+          <input name="username" type="text" value={this.state.username} onChange={this.handleChange} />
+          <input name="password" type="password" value={this.state.password} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Sign In" />
         {/* <p>{this.state.username}</p>
         <p>{this.state.password}</p> */}
 
@@ -51,4 +50,4 @@ class SignInForm extends React.Component {
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<SignInForm/>, root);
+ReactDOM.render(<SignInForm />, root);
